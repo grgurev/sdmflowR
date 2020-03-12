5ad301e675afc30bb0d7db070cf74166a13f5f77
Marin Grgurev <marin.grgurev@gmail.com>
2020-03-12 11:14:04 +0100

    Add roxygen header to document prepare_rasters()

--- R/prepare_rasters.R
+++ R/prepare_rasters.R
@@ -1,3 +1,16 @@
-prepare_rasters = function(folder) {
+#' Prepare rasters
+#'
+#' Prepare environmental raster layers by loading them from the folder and
+#' cropping to the project extent. In that process rasters are aligned to
+#' the same grid and extent and converted to ASCII rasters.
+#'
+#' @param folder filename (character) or Raster object. Supported file types
+#' are the ’native’ raster package format and those that can be read via rgdal
+#' @param extent Extent object, or any object from which an Extent object can
+#' be extracted
+#'
+#' @return RasterStack
+#' @export
+prepare_rasters = function(folder, extent) {
   list.files(folder)
 }
