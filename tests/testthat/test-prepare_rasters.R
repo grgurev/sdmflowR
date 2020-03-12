710951a2209af2385f3cbaeedda539cfbeb2c445
Marin Grgurev <marin.grgurev@gmail.com>
2020-03-12 17:40:18 +0100

    Edit prepare_rasters() test

--- tests/testthat/test-prepare_rasters.R
+++ tests/testthat/test-prepare_rasters.R
@@ -1,3 +1,14 @@
-test_that("prepare_rasters return list of files", {
-  expect_gt(length(prepare_rasters("./")), 0)
+test_that("prepare_rasters return croppped asci raster", {
+  f_in = tempdir()
+  f_out = f_in
+
+  r = raster::raster(nrows=10, ncols=10, xmn=0, xmx=10, ymn=0, ymx=10, vals=1)
+  raster::writeRaster(r, file.path(f_in, "test_raster.tif"), format="GTiff", overwrite=TRUE)
+
+  e = raster::extent(c(0, 2, 0, 2))
+  prepare_rasters(f_in, f_out, extent = e)
+  res = raster::raster(file.path(f_in, "test_raster.asc"))
+
+  expect_equal(res@extent, e)
+  rm(f_in, f_out, r, e, res)
 })
