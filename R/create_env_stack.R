9898857300e89bd0ea380926cb38e84c2ee93bb3
Marin Grgurev <marin.grgurev@gmail.com>
2020-07-17 15:53:49 +0200

    Correct small error in roxygen comment

--- R/create_env_stack.R
+++ R/create_env_stack.R
@@ -1,15 +1,15 @@
 #' Create raster stack from environmental data
 #' 
-#' Create raster stack from environmental data so that raster stack by 
+#' Create raster stack from environmental data so that raster stack by
 #' providing folder where raster are stored and optionally subset of those
 #' raster that needs processing. It is mostly used when checking for NAs and in
 #' value extraction in generating absences and psudoabsences
 #' 
 #' @param folder_in filename (character) or Raster object. Supported file types
 #' are the "native" raster package format and those that can be read via rgdal
-#' @param env_subset vector of filenames without extension (character) that will be 
-#' processed and stacked
-#' 
+#' @param env_subset vector of filenames without extension (character) that
+#' will be processed and stacked
+#'
 #' @return Raster stack
 #' @export
 #' @importFrom magrittr "%>%"
@@ -27,7 +27,7 @@ create_env_stack = function(folder_in, env_subset = c()) {
   # get the rasters and create raster stack
   stack = list.files(gsub("/$", "", folder_in), pattern, full.names = TRUE) %>%
     raster::stack()
-  
+
   # check if subset is defined to return only subset rasters
   if (length(env_subset) != 0) {
     stack = raster::subset(stack, env_subset)
