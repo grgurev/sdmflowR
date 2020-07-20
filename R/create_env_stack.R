9f199ce920fd85c24c928d1912d534c1602cd012
Marin Grgurev <marin.grgurev@gmail.com>
2020-07-20 17:12:18 +0200

    Refactor prepare_rasters and small corrections in other functions

--- R/create_env_stack.R
+++ R/create_env_stack.R
@@ -1,10 +1,12 @@
 #' Create raster stack from environmental data
-#' 
+#'
 #' Create raster stack from environmental data so that raster stack by
 #' providing folder where raster are stored and optionally subset of those
-#' raster that needs processing. It is mostly used when checking for NAs and in
-#' value extraction in generating absences and psudoabsences
-#' 
+#' raster that needs processing. It is just a wrapper around raster::stack()
+#' and used when you have subset of variables to try so that you can quickly
+#' check for NAs with `check_na_data()` function or value extraction when
+#' generating absences and psudoabsences
+#'
 #' @param folder_in filename (character) or Raster object. Supported file types
 #' are the "native" raster package format and those that can be read via rgdal
 #' @param env_subset vector of filenames without extension (character) that
@@ -14,7 +16,10 @@
 #' @export
 #' @importFrom magrittr "%>%"
 
-create_env_stack = function(folder_in, env_subset = c()) {
+create_env_stack = function(
+  folder_in,
+  env_subset = c()
+) {
   # set magrittr dot variable to NULL
   . = NULL
 
@@ -25,12 +30,21 @@ create_env_stack = function(folder_in, env_subset = c()) {
   pattern = "\\.bil$|\\.tif$|\\.asc$"
 
   # get the rasters and create raster stack
-  stack = list.files(gsub("/$", "", folder_in), pattern, full.names = TRUE) %>%
+  stack = list.files(
+    gsub(
+      pattern = "/$",
+      replacement = "",
+      x = folder_in),
+    pattern,
+    full.names = TRUE
+  ) %>%
     raster::stack()
 
   # check if subset is defined to return only subset rasters
   if (length(env_subset) != 0) {
-    stack = raster::subset(stack, env_subset)
+    stack = raster::subset(
+      x = stack,
+      env_subset)
   }
 
   # set the crs of raster stack
--- R/prepare_rasters.R
+++ R/prepare_rasters.R
@@ -2,7 +2,7 @@
 #'
 #' Prepare environmental raster layers by loading them from the folder and
 #' cropping to the project extent. In that process rasters are aligned to
-#' the same grid and extent and converted to ASCII rasters. This function works 
+#' the same grid and extent and converted to ASCII rasters. This function works
 #' only for continuous rasters.
 #'
 #' @param folder_in filename (character) or Raster object. Supported file types
@@ -32,42 +32,103 @@ prepare_rasters = function(folder_in, folder_out = folder_in, extent, ref_raster
   # default projection is WGS84
   default_proj = "+proj=longlat +datum=WGS84"
 
-  # create sf object from the project extent
+  # create sp object from the project extent
   extent_sp = methods::as(raster::extent(extent), 'SpatialPolygons')
-  raster::crs(extent_sp) <- raster::crs(default_proj)
+  raster::crs(extent_sp) = raster::crs(default_proj)
 
   # define pattern based on most often used formats
   pattern = "\\.bil$|\\.tif$"
 
   # read the files from folder where original rasters are stored
-  files_in = list.files(gsub("/$", "", folder_in), pattern = pattern, full.names = TRUE)
+  files_in = list.files(
+    gsub(
+      pattern = "/$",
+      replacement = "",
+      x = folder_in
+    ),
+    pattern = pattern,
+    full.names = TRUE
+  )
 
   # prepare filenames for prepared rasters
-  files_out = gsub(folder_in, "", files_in) %>%
-    gsub(pattern, ".asc", .) %>%
-    paste0(ifelse(!endsWith(folder_out, "/"), paste0(folder_out, "/"), folder_out), .)
+  files_out = gsub(
+    pattern = folder_in,
+    replacement = "",
+    x = files_in
+  ) %>%
+    gsub(
+      pattern = pattern,
+      replacement = ".asc",
+      x = .
+    ) %>%
+    paste0(
+      ifelse(
+        !endsWith(
+          x = folder_out,
+          suffix = "/"),
+        paste0(
+          folder_out,
+          "/"
+        ),
+      folder_out
+    ),
+  .)
 
   # load reference raster
   ref_raster = raster::raster(ref_raster) %>%
-    raster::crop(., raster::extent(extent_sp))
+    raster::crop(
+      x = .,
+      y = raster::extent(extent_sp)
+    )
 
   # read, crop and save rasters
   for (i in 1:length(files_in)) {
     # get the raster projection
-    raster_proj = raster::crs(raster::raster(files_in[i]))@projargs
+    raster_proj = raster::crs(
+      raster::raster(
+        x = files_in[i]
+      )
+    )@projargs
 
     # prepare raster by raster - load, project and resample
     raster::raster(files_in[i]) %>%
       {
         if(default_proj == raster_proj) {
-          raster::crop(., raster::extent(extent_sp)) %>%
-            raster::resample(., ref_raster, method = "ngb")
+          raster::crop(
+            x = .,
+            y = raster::extent(extent_sp)
+          ) %>%
+            raster::resample(
+              x = .,
+              y = ref_raster,
+              method = "ngb"
+            )
         } else {
-          raster::crop(., raster::extent(sp::spTransform(extent_sp, raster_proj))) %>%
-            raster::projectRaster(., ref_raster, method = "bilinear") %>%
-            raster::resample(., ref_raster, method = "ngb")
+          raster::crop(
+            x = .,
+            y = raster::extent(
+              x = sp::spTransform(
+                x = extent_sp,
+                CRSobj = raster_proj
+              )
+            )
+          ) %>%
+            raster::projectRaster(
+              from = .,
+              to = ref_raster,
+              method = "bilinear"
+            ) %>%
+            raster::resample(
+              x = .,
+              y = ref_raster,
+              method = "ngb"
+            )
         }
       } %>%
-      raster::writeRaster(., paste0(files_out[i]), overwrite=TRUE)
+      raster::writeRaster(
+        x = .,
+        filename = paste0(files_out[i]),
+        overwrite=TRUE
+      )
   }
 }
